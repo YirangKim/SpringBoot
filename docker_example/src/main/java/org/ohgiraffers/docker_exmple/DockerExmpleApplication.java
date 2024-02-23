6af43902e8189234183bfcaba17ae9146fd0474f
DESKTOP-V05N2DI\804-10 <olrang.2@gmail.com>
2024-02-23 18:30:57 +0900

    0223 docker

---
+++ docker_example/src/main/java/org/ohgiraffers/docker_exmple/DockerExmpleApplication.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.docker_exmple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerExmpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerExmpleApplication.class, args);
+    }
+
+}
---
+++ docker_example/src/test/java/org/ohgiraffers/docker_exmple/DockerExmpleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.docker_exmple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerExmpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
