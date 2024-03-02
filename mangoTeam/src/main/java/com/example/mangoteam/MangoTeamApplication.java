f3c39472f5d0133ddc8a288bf594ac7b4fd55977
YirangKim <113574030+YirangKim@users.noreply.github.com>
2024-03-02 23:13:53 +0900

    [feat] yml test

---
+++ mangoTeam/src/main/java/com/example/mangoteam/MangoTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.mangoteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangoTeamApplication.class, args);
+    }
+
+}
---
+++ mangoTeam/src/test/java/com/example/mangoteam/MangoTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mangoteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
