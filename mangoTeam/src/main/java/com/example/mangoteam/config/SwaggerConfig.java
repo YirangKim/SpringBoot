d8ada886aa351e6bb54c9dabc9c723ce9595df5f
YirangKim <113574030+YirangKim@users.noreply.github.com>
2024-03-03 21:19:17 +0900

    [Feat] SwaggerConfig 세팅

---
+++ mangoTeam/src/main/java/com/example/mangoteam/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.example.mangoteam.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Board Mission😉",
+                description = "Board Mission Api 명세",
+                version = "v1")
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi firstOpenApi(){
+        String[] path = {
+                "org.ohgiraffers.board.controller"
+        };
+
+        return GroupedOpenApi.builder()
+                .group("1. 게시글 관리")
+                .packagesToScan(path)
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi secondOpenApi(){
+        String[] path = {
+                ""
+        };
+
+        return GroupedOpenApi.builder()
+                .group("2. 아직 미정")
+                .packagesToScan(path)
+                .build();
+    }
+
+}
